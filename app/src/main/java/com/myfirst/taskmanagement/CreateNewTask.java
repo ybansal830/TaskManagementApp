8fc826108292102309f0bd1644e02ce4ae4e73db
Yash Bansal <ybansalnaveen@gmail.com>
2021-07-06 00:48:15 +0530

    first

---
+++ app/src/main/java/com/myfirst/taskmanagement/CreateNewTask.java
@@ -0,0 +1,14 @@
+package com.myfirst.taskmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNewTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_task);
+    }
+}
